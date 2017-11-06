085e065bc4761256588c52c076c998d121e84fea
NicoWindows <delimanicolasgustavo@gmail.com>
2017-11-06 10:45:38 -0300

    Agrego form15 y nico.js, estilo necesario en nico.css y modificacion de navegacion en form14

---
+++ js/nico.js
@@ -0,0 +1,50 @@
+function barricadaTextArea(){
+	var detalle = document.getElementById("barricadaDetalle");
+	if(detalle.style.display==="none") 
+	{
+		detalle.style.display="block";
+	}
+	else 
+	{
+		detalle.style.display="none";
+	}
+}
+
+function geotecnicaTextArea()
+{
+	var detalle = document.getElementById("geotecnicaDetalle");
+	if(detalle.style.display==="none") 
+	{
+		detalle.style.display="block";
+	}
+	else 
+	{
+		detalle.style.display="none";
+	}
+}
+
+function estructuralTextArea()
+{
+	var detalle = document.getElementById("estructuralDetalle");
+	if(detalle.style.display==="none") 
+	{
+		detalle.style.display="block";
+	}
+	else 
+	{
+		detalle.style.display="none";
+	}
+}
+
+function otroTextArea()
+{
+	var detalle = document.getElementById("otroDetalle");
+	if(detalle.style.display==="none") 
+	{
+		detalle.style.display="block";
+	}
+	else 
+	{
+		detalle.style.display="none";
+	}
+}

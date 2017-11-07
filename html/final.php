ec5c2e5d43a0fbdb8e48edb1d8b9885114b30508
sebasegura97 <sebasegura97@gmail.com>
2017-11-07 10:06:53 -0300

    CONECTANDO A BD

---
+++ html/final.php
@@ -0,0 +1,17 @@
+<?php
+
+// CONEXION
+
+  $connectionInfo = array("UID" => "proyectosismos@proyectosismos", "pwd" => "Waxelrose23", "Database" => "proyectosismos", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+  $serverName = "tcp:proyectosismos.database.windows.net,1433";
+  $conn = sqlsrv_connect($serverName, $connectionInfo);
+
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+      echo "No conectado";
+  }
+  echo "Connected successfully <br>";
+
+  
+  var_dump($_COOKIE);
+ ?>
